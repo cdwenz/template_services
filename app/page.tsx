0e3feb2e5915418786c9f0964b72f0ede0277f1b
Cristian <cdwenz@gmail.com>
2024-10-24 12:24:27 -0300

    dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ import LandingFooter from "@/components/LandingPage/Landing_Footer/LandingFooter
 import LandingContact from "@/components/LandingPage/Landing_Contact/LandingContact";
 import WA from "@/components/Whatsapp_Float/WA";
 import LandingServices from "@/components/LandingPage/Landing_Services/LandingServices";
-import LandingGallery from "@/components/LandingPage/Landing_Gallery/LandingGallery";
-import LandingSlides from "@/components/LandingPage/Landing_Slides/LandingSlides";
+// import LandingGallery from "@/components/LandingPage/Landing_Gallery/LandingGallery";
+import dynamic from 'next/dynamic';
+
+// Carga dinámica del componente Chart
+const Slides = dynamic(() => import('@/components/LandingPage/Landing_Slides/LandingSlides'));
+const Gallery = dynamic(() => import('@/components/LandingPage/Landing_Gallery/LandingGallery'));
 
 function page() {
   return (
@@ -21,9 +25,9 @@ function page() {
           style={{ height: "25px", border: "none", backgroundColor: "#202020" }}
         />
       </>
-      <LandingSlides />
+      <Slides />
       <LandingAbout />
-      <LandingGallery />
+      <Gallery />
       <LandingContact />
       <LandingFooter />
       <WA />
