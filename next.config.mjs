a89d01a6e266617880bea4ef039d9b8a496a7b14
Cristian <cdwenz@gmail.com>
2024-10-24 11:50:07 -0300

    cache icon8

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["icon8.com"], // Agrega los dominios que usas para tus imágenes externas
+  },
+};
 
 export default nextConfig;
